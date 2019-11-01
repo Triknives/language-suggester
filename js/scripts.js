d84fd11d4b2d65388ca2e38d22f71fce6474fc2e
Triknives <50305423+Triknives@users.noreply.github.com>
2019-10-31 19:33:51 -0700

    update branching functionality allowing proper results from survey. Edits to branching lines and spacing, to increase readability.

--- js/scripts.js
+++ js/scripts.js
@@ -5,29 +5,57 @@ $(document).ready(function() {
     var fun = $("input:radio[name=fun]:checked").val();
     var colors = $("input:radio[name=colors]:checked").val();
     var style = $("input:radio[name=style]:checked").val();
-    var batman = $("input:radio[name=interest]:checked").val();
+    var batman = $("input:radio[name=batman]:checked").val();
 
     $(".resultsbox").hide();
 
     event.preventDefault();
 
     if (interest === "hamburger" && fun === "kit" && style === "complex"){
-      $("#language").text("Swift"); $(".resultsbox").show();
+      $("#language").text("Swift");
+
+      $(".resultsbox").show();
     }
     else if (interest === "hotdog" && fun === "tesla" && style === "simple"){
-      $("#language").text("Go");$(".resultsbox").show();
+      $("#language").text("Go");
+
+      $(".resultsbox").show();
     }
     else if (interest === "altoid" && fun === "volkswagen" && style === "established"){
-      $("#language").text("Ruby");$(".resultsbox").show();
+      $("#language").text("Ruby");
+
+      $(".resultsbox").show();
     }
     else if (interest === "hamburger" && fun === "kit" && colors === "green"){
-      $("#language").text("Swift");$(".resultsbox").show();
-      }
-    else if (interest === "altoid" && fun === "tesla" && style === "established"){
-      $("#language").text("Ruby");$(".resultsbox").show();
+      $("#language").text("Swift");
+
+      $(".resultsbox").show();
     }
-    else if (interest === "hamburger" && fun === "tesla" && style === "complex"){
-        $("#language").text("Go");$(".resultsbox").show();
+    else if (interest === "altoid" && fun === "tesla" && style === "established"){
+      $("#language").text("Ruby");
+
+      $(".resultsbox").show();
+    }
+    else if (batman === "affleck" && fun === "tesla" && style === "complex"){
+      $("#language").text("Swift");
+
+      $(".resultsbox").show();
+    }
+    else if (batman === "bale" && fun === "tesla" && style === "complex"){
+      $("#language").text("Go");
+
+      $(".resultsbox").show();
+    }
+    else if (batman === "keaton" && fun === "tesla" && style === "complex"){
+      $("#language").text("Ruby");
+
+      $(".resultsbox").show();
+    }
+    else
+    {
+      $("#language").text("Ruby");
+
+      $(".resultsbox").show();
     };
   });
 });
